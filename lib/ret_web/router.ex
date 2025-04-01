0d649df411667f2ece51c8158895ff9d9e380839
Alex Lennon <ajlennon@dynamicdevices.co.uk>
2025-04-01 19:38:51 +0100

    Make things insecure!
    As per these instructions
    
    https://github.com/albirrkarim/mozilla-hubs-installation-detailed#46-run-hubs-admin

--- lib/ret_web/router.ex
+++ lib/ret_web/router.ex
@@ -95,7 +95,10 @@ defmodule RetWeb.Router do
   end
 
   scope "/api/postgrest" do
-    pipe_through [:secure_headers, :auth_required, :admin_required, :proxy_api]
+#    pipe_through [:secure_headers, :auth_required, :admin_required, :proxy_api]
+    if(Mix.env() == :prod) do
+    pipe_through([:secure_headers])
+    end
 
     forward "/", RetWeb.Plugs.PostgrestProxy
   end
